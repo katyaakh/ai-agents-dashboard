4fcae29fe5e2022351c1e69343d787c7209476ab
Katya Akh <ekatakhm@gmail.com>
2025-09-03 18:01:47 +0200

    Remove circular styling from agent images - make them rectangular/square

--- WelcomePageWithAgent.vue
+++ WelcomePageWithAgent.vue
@@ -657,10 +657,7 @@ const checklistItem2 = ref(false)
 .agent-image {
   width: 100%;
   height: 100%;
-  border-radius: 50%;
   object-fit: cover;
-  border: 3px solid white;
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
 }
 
 /* Section 2: Statistics */
